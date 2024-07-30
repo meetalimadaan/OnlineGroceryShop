70a30c6040dd1cd58b960a196f283e479b2c93a9
meetalimadaan <meetali@infostride.com>
2024-07-30 13:10:11 +0530

    CartScreen Code Updated

--- OnlineGroceriesApp/UICommom/CartItemRow.swift
+++ OnlineGroceriesApp/UICommom/CartItemRow.swift
@@ -18,29 +18,81 @@ struct CartItemRow: View {
                     .frame(width: 60, height: 60)
                 
                 VStack(spacing:4){
-                    Text("Banana")
-                        .font(.customfont(.bold, fontSize: 16))
-                        .foregroundColor(.primaryText)
-                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                    
+                    HStack{
+                        Text("Banana")
+                            .font(.customfont(.bold, fontSize: 16))
+                            .foregroundColor(.primaryText)
+                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                        
+                        Button{
+                            
+                        } label: {
+                            Image("Group 6862")
+                                .resizable()
+                                .frame(width: 18, height: 18)
+                        }
+                    }
+                    
                     
                     Text("7pcs, price")
                         .font(.customfont(.medium, fontSize: 14))
                         .foregroundColor(.secondaryText)
                         .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                        .padding(.bottom, 8)
+                    
+                    
+                    HStack{
+                        
+                        Button{
+                            
+                        } label: {
+                            Image("Vector-5")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 20, height: 20)
+                                .padding(10)
+                        }
+                        .padding(4)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 16)
+                                .stroke(Color.secondaryText.opacity(0.5), lineWidth: 1)
+                        )
+                        
+                        Text("1")
+                            .font(.customfont(.semibold, fontSize: 24))
+                            .foregroundColor(.primaryText)
+                            .multilineTextAlignment(.center)
+                            .frame(width: 45, height: 45, alignment: .center)
+//                            .overlay(
+//                                RoundedRectangle(cornerRadius: 16)
+//                                    .stroke(Color.secondaryText.opacity(0.5), lineWidth: 1)
+//                            )
+                     
+                        Button{
+                            
+                        } label: {
+                            Image("Vector-6")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 20, height: 20)
+                                .padding(10)
+                        }
+                        .padding(4)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 16)
+                                .stroke(Color.secondaryText.opacity(0.5), lineWidth: 1)
+                        )
+                        
+                        Spacer()
+                        
+                        Text("$2.99 ")
+                            .font(.customfont(.semibold, fontSize: 18))
+                            .foregroundColor(.primaryText)
+                    }
                 }
                 
-                Text("$2.99 ")
-                    .font(.customfont(.semibold, fontSize: 18))
-                    .foregroundColor(.primaryText)
-                //                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .trailing)
-                
-                Image("back arrow 1")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 15, height: 15)
-                
-                //                            Text("Favourites")
-                //                                 .font(.customfont(.bold, fontSize: 20))
+               
             }
             Divider()
         }
@@ -48,4 +100,5 @@ struct CartItemRow: View {
 }
 #Preview {
     CartItemRow()
+        .padding(.horizontal, 20)
 }
